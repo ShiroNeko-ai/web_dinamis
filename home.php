de777a4ee5d028e3ae0eaf3b2265325b51f6e758
Amnapp <67993450+ShiroNeko-ai@users.noreply.github.com>
2022-03-15 11:57:00 +0700

    Add files via upload

---
+++ home.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+</head>
+<body>
+
+<table border="1">
+    <thead>
+                <tr>
+                    <th>no</th>
+                    <th>nama</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>1</td>
+                    <td>Ahnaf Musyaffa</td>
+                </tr>
+            </tbody>
+        </table>
+
+</body>
+</html>
